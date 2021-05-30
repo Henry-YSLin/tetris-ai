5f0f46db50f2f2ffdc2fc9d579aaa59d46395e6f
Henry Lin <henry.ys.lin@gmail.com>
2021-05-30 11:17:00 +0800

    More specific eslint disable comments

--- src/tetris/utils/Mixin.ts
+++ src/tetris/utils/Mixin.ts
@@ -1,4 +1,5 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
 export type Constructor<T = {}> = new (...args: any[]) => T;
 
 export type Subtract<T, U> = T & Exclude<T, U>;
