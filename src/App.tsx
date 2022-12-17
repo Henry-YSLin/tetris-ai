d0c8a6cef11d0592fcb925b01f3f5b519668469e
Henry Lin <henry.ys.lin@gmail.com>
2022-12-17 16:28:14 +0800

    Migrate to vite, update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Error404 from './pages/Error404';
 import Home from './pages/Home';
 import LocalMultiGame from './pages/LocalMultiGame';
 import TestGame from './pages/TestGame';
 
-const App: React.FC = () => {
-	return (
-		<HashRouter>
-			<Switch>
-				<Route exact path='/' component={Home} />
-				<Route exact path='/test' component={TestGame} />
-				<Route exact path='/multi/local' component={LocalMultiGame} />
-				<Route component={Error404} />
-			</Switch>
-		</HashRouter>
-	);
-};
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error404 />,
+  },
+  {
+    path: '/test',
+    element: <TestGame />,
+  },
+  {
+    path: '/multi/local',
+    element: <LocalMultiGame />,
+  },
+]);
+
+const App: React.FC = () => <RouterProvider router={router} />;
 
 export default App;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: 'src',
+  base: '/',
+  publicDir: '../public',
+  plugins: [react()],
+  build: {
+    // Specify the dist folder
+    outDir: '../dist',
+  },
+});
