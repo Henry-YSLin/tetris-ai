45321749206bde5763c77cb608da8ba0fb524f27
Henry Lin <henry.ys.lin@gmail.com>
2022-12-22 18:52:38 +0800

    Change home page style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center select-none min-h-screen bg-gradient-to-br from-gray-900  to-blue-700">
       <h1 className="text-6xl text-green-500 border-b-4 pb-4">Hello World !</h1>
-      <h2 className="text-2xl text-gray-300 mt-10">
+      <h2 className="text-4xl text-gray-300 mt-10 text-center">
         <Link to="/solo" className="text-green-400 underline">
           Single player
         </Link>
